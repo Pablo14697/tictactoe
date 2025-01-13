95fd1a767328fe565cefec9260bd0de872694e76
Pablo Medina <pablomed88@gmail.com>
2025-01-13 16:20:08 -0300

    PR-2 randobot tests added

--- apps/web/src/components/pages/game/game.page.test.tsx
+++ apps/web/src/components/pages/game/game.page.test.tsx
@@ -1,7 +1,15 @@
 import { testIdActionButton, testIdCell, testIdGameStatus } from '@constants';
 import { GamePage } from '@pages/game/game.page';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+
+const generateCpuPlayMock = vi.hoisted(() => vi.fn());
+
+vi.mock('@utils/generate-cpu-play.util', () => {
+  return {
+    generateCpuPlay: generateCpuPlayMock,
+  };
+});
 
 type CellIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 type ResetButton = 'reset';
@@ -19,22 +27,52 @@ const playGame = (clicks: Clickable[]): string | undefined => {
   return screen.getByTestId(testIdGameStatus)?.textContent ?? undefined;
 };
 
-describe('Tic-Tac-Toe Game', () => {
-  const games: { clicks: Clickable[]; expected: string }[] = [
-    { clicks: [0, 3, 1, 4, 2], expected: 'Winner: X' }, // top row
-    { clicks: [2, 4, 5, 3, 8], expected: 'Winner: X' }, // middle column
-    { clicks: [0, 2, 1, 4, 3, 6], expected: 'Winner: O' }, // diagonal
-    { clicks: [0, 1, 3, 4, 7, 6, 2, 5, 8], expected: "It's a Draw" },
-    { clicks: [0], expected: 'Next Turn: O' },
-    { clicks: [0, 1], expected: 'Next Turn: X' },
-    { clicks: [0, 1, 1], expected: 'Next Turn: X' },
-    { clicks: [0, 1, 'reset', 0], expected: 'Next Turn: O' },
-  ];
+const multiplayerGamesMock: { clicks: Clickable[]; expected: string }[] = [
+  { clicks: [0, 3, 1, 4, 2], expected: 'Winner: X' }, // top row
+  { clicks: [2, 4, 5, 3, 8], expected: 'Winner: X' }, // middle column
+  { clicks: [0, 2, 1, 4, 3, 6], expected: 'Winner: O' }, // diagonal
+  { clicks: [0, 1, 3, 4, 7, 6, 2, 5, 8], expected: "It's a Draw" },
+  { clicks: [0], expected: 'Next Turn: O' },
+  { clicks: [0, 1], expected: 'Next Turn: X' },
+  { clicks: [0, 1, 1], expected: 'Next Turn: X' },
+  { clicks: [0, 1, 'reset', 0], expected: 'Next Turn: O' },
+];
 
-  it.each(games)(
-    'should display $expected after clicks: $clicks',
+const cpuGamesMock: { clicks: number[]; expected: string }[] = [
+  { clicks: [0, 3, 1, 4, 2], expected: 'Winner: X' }, // top row
+  { clicks: [2, 4, 5, 3, 8], expected: 'Winner: X' }, // middle column
+  { clicks: [0, 2, 1, 4, 3, 6], expected: 'Winner: O' }, // diagonal
+  { clicks: [0, 1, 3, 4, 7, 6, 2, 5, 8], expected: "It's a Draw" },
+  { clicks: [0], expected: 'Next Turn: X' },
+];
+
+describe('Tic-Tac-Toe Game', () => {
+  it.each(multiplayerGamesMock)(
+    'should display $expected after clicks: $clicks for Multiplayer mode',
     async ({ clicks, expected }) => {
       expect(playGame(clicks)).toBe(expected);
+    },
+  );
+
+  it.each(cpuGamesMock)(
+    'should display $expected after clicks: $clicks for CPU mode',
+    async ({ clicks, expected }) => {
+      const oddClicks = clicks.filter((_, index) => index % 2 !== 0);
+      const evenClicks = clicks.filter((_, index) => index % 2 === 0);
+
+      for (const click of oddClicks) {
+        vi.mocked(generateCpuPlayMock).mockReturnValueOnce(click);
+      }
+
+      render(<GamePage />);
+
+      fireEvent.click(screen.getByTestId('cpu-action-button'));
+
+      for (const click of evenClicks) {
+        fireEvent.click(screen.getByTestId(testIdCell(click)));
+      }
+
+      expect(screen.getByTestId(testIdGameStatus)?.textContent).toBe(expected);
     },
   );
 });
